6a729770661fd4ef1198477651e32b606feaf9b7
hilman khitam <hilmankhitam100@gmail.com>
2023-11-16 11:01:38 +0800

    listing creation, location selection & map component & country autocomplete

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,12 +2,14 @@
 
 import { useMemo, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import dynamic from "next/dynamic";
 
 import useRentModal from "@/app/hooks/useRentModal"
 import Modal from "./Modal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
     CATEGORY = 0,
@@ -46,6 +48,11 @@ const RentModal = () => {
     });
 
     const category = watch('category');
+    const location = watch('location');
+
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }), [location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -100,6 +107,22 @@ const RentModal = () => {
         </div>
     );
 
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Where is your place located?"
+                    subtitle="Help guests find you!"
+                />
+                <CountrySelect
+                    onChange={(value) => setCustomValue('location', value)}
+                    value={location}
+                />
+                <Map center={location?.latlng} />
+            </div>
+        )
+    }
+
     return (
         <Modal
             isOpen={rentModal.isOpen}
@@ -114,4 +137,4 @@ const RentModal = () => {
     )
 }
 
-export default RentModal
+export default RentModal;
