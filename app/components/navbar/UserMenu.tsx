bef4c83e4eee9ff2a9d8d0fcca0823428577afe1
hilman khitam <hilmankhitam100@gmail.com>
2023-11-25 08:42:45 +0800

    Reservatios screen

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -61,7 +61,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                                 <MenuItem onClick={() => router.push('/trips')} label='My trips' />
                                 <MenuItem onClick={() => { }}
                                     label='My favorites' />
-                                <MenuItem onClick={() => { }}
+                                <MenuItem onClick={() => router.push('/reservations')}
                                     label='My reservations' />
                                 <MenuItem onClick={() => { }}
                                     label='My properties' />
